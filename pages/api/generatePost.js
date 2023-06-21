8d6419fe409e84979517b913127cc314732f9f71
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-06-21 19:06:30 -0400

    Update model w/ gpt3.5

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -8,15 +8,89 @@ export default async function handler(req, res) {
 
     const openai = new OpenAIApi(config);
 
+    const {topic, keywords} = req.body;
+
+    // const topic = "Top 10 tips for dog owners";
+    // const keywords = "dog, tips, training, food, health, exercise, grooming, vet, socialization, love";
+
     // very slow
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
+    // const response = await openai.createCompletion({
+    //     model: "text-davinci-003",
+    //     temperature: 0,
+    //     max_tokens: 3600,
+    //     prompt: `Generate a post about ${topic}, using the following keywords: ${keywords}. The content should be formatted in SEO-friendly HTML. The response must also include appropriate HTML title and meta description contents. The return format must be stringified JSON in the following format:{
+    //         "postContent": post content here,
+    //         "title": title here,
+    //         "metaDescription": meta description here
+    //     }.`
+    // });
+
+    const postContentResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
         temperature: 0,
-        max_tokens: 3600,
-        prompt: "Generate a post about owning dogs",
-    });
+        messages: [{
+            role: "system",
+            content: "You are a blog post generator.",
+        },
+        {
+            role:"user",
+            content:`Generate a post about ${topic}, using the following keywords: ${keywords}. The content should be formatted in SEO-friendly HTML. limited to the following HTML tags:p, h1, h2, h3, h4, h5, li, ol, strong, ul, i.`
+        }]
+    })
 
-    console.log('Response', response);
+    const postContent = postContentResponse.data.choices[0]?.message?.content || "";
 
-    res.status(200).json({ post: response.data.choices })
+    const titleResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0,
+        messages: [{
+            role: "system",
+            content: "You are a blog post generator.",
+        },
+        {
+            role:"user",
+            content:`Generate a post about ${topic}, using the following keywords: ${keywords}. The content should be formatted in SEO-friendly HTML. limited to the following HTML tags:p, h1, h2, h3, h4, h5, li, ol, strong, ul, i.`
+        },{
+            role: "assistant",
+            content: postContent,
+        },{
+            role: "user",
+            content: "Generate an appropriate title tag text for the above blog post."
+        }]
+    })
+
+    const metaDescriptionResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0,
+        messages: [{
+            role: "system",
+            content: "You are a blog post generator.",
+        },
+            {
+                role:"user",
+                content:`Generate a post about ${topic}, using the following keywords: ${keywords}. The content should be formatted in SEO-friendly HTML. limited to the following HTML tags:p, h1, h2, h3, h4, h5, li, ol, strong, ul, i.`
+            },{
+                role: "assistant",
+                content: postContent,
+            },{
+                role: "user",
+                content: "Generate SEO-friendly meta description text for the above blog post."
+            }]
+    })
+
+    const title = titleResponse.data.choices[0]?.message?.content || "";
+    const metaDescription = metaDescriptionResponse.data.choices[0]?.message?.content || "";
+
+    console.log('PostContent:', postContent);
+    console.log('Title:', title);
+    console.log('MetaDescription:', metaDescription);
+    //console.log('Response', response?.choices);
+
+    res.status(200).json({
+        post: {
+            postContent,
+            title,
+            metaDescription
+        }
+    })
 }
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,22 +1,60 @@
 import {withPageAuthRequired} from "@auth0/nextjs-auth0";
 import {AppLayout} from "../../components";
+import {useState} from "react";
 
 export default function NewPost(props) {
     console.log(props);
-    const handleClick = async () =>{
+    const [postContent, setPostContent] = useState("");
+    const [topic, setTopic] = useState("");
+    const [keywords, setKeywords] = useState("");
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
         const res = await fetch(`/api/generatePost`,{
             method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                topic,
+                keywords
+            })
         });
         const json = await res.json();
-        console.log('Result:',json);
+        console.log('Result:',json.post.postContent);
+        setPostContent(json.post.postContent);
     }
 
     return (
         <div>
-            <h1>Post Playground</h1>
-            <button className={"btn"} onClick={handleClick}>
-                Generate Post
-            </button>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <lable>
+                        <strong>Generate a blog post on the topic of</strong>
+                    </lable>
+                    <textarea
+                        className={"resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-md"}
+                        value={topic}
+                        onChange={e=>setTopic(e.target.value)} />
+                </div>
+                <div>
+                    <lable>
+                        <strong>Targeting the following keywords:</strong>
+                    </lable>
+                    <textarea
+                        className={"resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-md"}
+                        value={keywords}
+                        onChange={e=>setKeywords(e.target.value)}/>
+                </div>
+                <button
+                    type={"submit"}
+                        className={"btn"}
+                        onSubmit={handleSubmit}>
+                    Generate Post
+                </button>
+            </form>
+            <div className={"max-w-screen-sm p-10"}
+                dangerouslySetInnerHTML={{__html: postContent}} />
         </div>);
 }
 
